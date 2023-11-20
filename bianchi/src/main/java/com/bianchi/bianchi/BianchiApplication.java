115132b09e5e4666299cb211ae38b28dd4cbcd6f
BianchiDev <ifpebianchi@gmail.com>
2023-11-20 00:07:57 -0300

    first commit

---
+++ bianchi/src/main/java/com/bianchi/bianchi/BianchiApplication.java
@@ -0,0 +1,13 @@
+package com.bianchi.bianchi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BianchiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BianchiApplication.class, args);
+	}
+
+}
---
+++ bianchi/src/test/java/com/bianchi/bianchi/BianchiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bianchi.bianchi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BianchiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
